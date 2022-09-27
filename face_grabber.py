08aebb683ffece18872558aede42f3a2e7042789
Lucas Zurverra <lzurverra@gmail.com>
2022-09-27 10:17:53 -0300

    changed variable names

--- face_grabber.py
+++ face_grabber.py
@@ -2,16 +2,16 @@ import cv2
 import os
 import imutils
 
-personName = input("> Type person name: ").capitalize()
-dataPath = "data"
-personPath = os.path.join(dataPath, personName.lower())
+person_name = input("> Type person name: ").capitalize()
+data_path = "data"
+person_path = os.path.join(data_path, person_name.lower())
 
-if not os.path.exists(personPath):
-	print("Folder created successfully: ", personPath)
-	os.makedirs(personPath)
+if not os.path.exists(person_path):
+	print("Folder created successfully: ", person_path)
+	os.makedirs(person_path)
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#cap = cv2.VideoCapture("Video.mp4")
+#cap = cv2.VideoCapture("video.mp4")
 
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 count = 0
@@ -21,16 +21,16 @@ while True:
 	if ret == False: break
 	frame =  imutils.resize(frame, width=640)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	auxFrame = frame.copy()
+	aux_frame = frame.copy()
 
 	faces = faceClassif.detectMultiScale(gray, 1.3, 5)
 
-	for (x,y,w,h) in faces:
+	for (x, y, w, h) in faces:
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		rostro = auxFrame[y:y+h,x:x+w]
+		rostro = aux_frame[y:y+h, x:x+w]
 		rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
-		cv2.imwrite(personPath + "/face_{}.jpg".format(count),rostro)
-		count = count + 1
+		cv2.imwrite(os.path.join(person_path, "face_{}.jpg").format(count),rostro)
+		count += 1
 	cv2.imshow("frame", frame)
 
 	k =  cv2.waitKey(1)
