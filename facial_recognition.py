d982973b9138f88aeb36dacf1a959b6150f7b6c4
Lucas Zurverra <lzurverra@gmail.com>
2022-09-30 17:09:00 -0300

    code reformated

--- facial_recognition.py
+++ facial_recognition.py
@@ -4,21 +4,22 @@ import os
 data_dir = "data"
 image_paths = os.listdir(data_dir)
 model_dir = "model"
-model_name = "modelLBPHFace.xml"
+
 # model_name = "modelEigenFace.xml"
 # model_name = "modelFisherFace.xml"
+model_name = "modelLBPHFace.xml"
 model_path = os.path.join(model_dir, model_name)
 print("image_paths:", image_paths)
 
-#face_recognizer = cv2.face.EigenFaceRecognizer_create()
-#face_recognizer = cv2.face.FisherFaceRecognizer_create()
+# face_recognizer = cv2.face.EigenFaceRecognizer_create()
+# face_recognizer = cv2.face.FisherFaceRecognizer_create()
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 # reading model
 face_recognizer.read(model_path)
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#cap = cv2.VideoCapture("video.mp4")
+# cap = cv2.VideoCapture("video.mp4")
 
 face_classif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
@@ -28,38 +29,38 @@ while True:
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	aux_frame = gray.copy()
 
-	faces = face_classif.detectMultiScale(gray,1.3,5)
+	faces = face_classif.detectMultiScale(gray, 1.3, 5)
 
 	for (x, y, w, h) in faces:
-		face = aux_frame[y:y+h,x:x+w]
+		face = aux_frame[y:y+h, x:x+w]
 		face = cv2.resize(face, (150, 150), interpolation=cv2.INTER_CUBIC)
 		result = face_recognizer.predict(face)
 
 		cv2.putText(frame, "{}".format(result), (x, y-5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
-		"""
+
 		# EigenFaces
 		if result[1] < 5700:
 			cv2.putText(frame, "{}".format(image_paths[result[0]]),(x, y-25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
 			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 		else:
-			cv2.putText(frame, "Desconocido", (x, y-20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
-		
-		# FisherFace
-		if result[1] < 500:
-			cv2.putText(frame, "{}".format(image_paths[result[0]]), (x, y-25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-		else:
-			cv2.putText(frame, "Desconocido", (x, y-20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
-		"""
-		# LBPHFace
-		if result[1] < 70:
-			cv2.putText(frame, "{}".format(image_paths[result[0]].capitalize()), (x, y-25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
-		else:
 			cv2.putText(frame, "Unknown", (x, y-20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
 			cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+
+		# # FisherFace
+		# if result[1] < 500:
+		# 	cv2.putText(frame, "{}".format(image_paths[result[0]]), (x, y-25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
+		# 	cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		# else:
+		# 	cv2.putText(frame, "Unknown", (x, y-20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+		# 	cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+
+		# # LBPHFace
+		# if result[1] < 70:
+		# 	cv2.putText(frame, "{}".format(image_paths[result[0]].capitalize()), (x, y-25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
+		# 	cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+		# else:
+		# 	cv2.putText(frame, "Unknown", (x, y-20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+		# 	cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
 		
 	cv2.imshow("frame", frame)
 	k = cv2.waitKey(1)
--- train_facial_recognition.py
+++ train_facial_recognition.py
@@ -5,9 +5,10 @@ import numpy as np
 data_dir = "data"
 people_list = os.listdir(data_dir)
 model_dir = "model"
-model_name = "modelLBPHFace.xml"
+
 # model_name = "modelEigenFace.xml"
 # model_name = "modelFisherFace.xml"
+model_name = "modelLBPHFace.xml"
 model_path = os.path.join(model_dir, model_name)
 print("People list:", people_list)
 
@@ -27,18 +28,18 @@ for name_dir in people_list:
 		print("Faces: ", os.path.join(name_dir, file_name))
 		labels.append(label)
 		faces_data.append(cv2.imread(os.path.join(person_path, file_name), 0))
-		#image = cv2.imread(os.path.join(person_path, file_name), 0)
-		#cv2.imshow("image", image)
-		#cv2.waitKey(10)
+		# image = cv2.imread(os.path.join(person_path, file_name), 0)
+		# cv2.imshow("image", image)
+		# cv2.waitKey(10)
 	label += 1
 
-#print("labels=", labels)
-#print("Labels number 0: ", np.count_nonzero(np.array(labels)==0))
-#print("Labels number 1: ", np.count_nonzero(np.array(labels)==1))
+# print("labels=", labels)
+# print("Labels number 0: ", np.count_nonzero(np.array(labels)==0))
+# print("Labels number 1: ", np.count_nonzero(np.array(labels)==1))
 
 # methods to train recognizer
-#face_recognizer = cv2.face.EigenFaceRecognizer_create()
-#face_recognizer = cv2.face.FisherFaceRecognizer_create()
+# face_recognizer = cv2.face.EigenFaceRecognizer_create()
+# face_recognizer = cv2.face.FisherFaceRecognizer_create()
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 # training face recognizer
